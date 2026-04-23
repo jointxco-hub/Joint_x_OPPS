9cf9869e1fb580a6f4c897c833880ef97e594e96
Jasper Jai <jointx.co@gmail.com>
2026-04-23 10:23:02 +0200

    fix: add Vite alias for @ and correct main.jsx imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   logLevel: 'error',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
